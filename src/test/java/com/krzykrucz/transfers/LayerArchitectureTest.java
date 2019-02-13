f548494f63e19e54dcf1b2bb7e1b0a602c988b2e
k.kruczynski <k.kruczynski@ocado.com>
2019-02-13 14:03:07 +0100

    adapters architecture test

--- src/test/java/com/krzykrucz/transfers/LayerArchitectureTest.java
+++ src/test/java/com/krzykrucz/transfers/LayerArchitectureTest.java
@@ -1,7 +1,9 @@
 package com.krzykrucz.transfers;
 
 
+import com.google.common.reflect.ClassPath;
 import com.krzykrucz.transfers.application.api.MoneyTransfersAPI;
+import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ImportOption.DontIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
@@ -9,11 +11,19 @@ import com.tngtech.archunit.junit.ArchUnitRunner;
 import com.tngtech.archunit.lang.ArchRule;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 @RunWith(ArchUnitRunner.class)
 @AnalyzeClasses(packages = "com.krzykrucz.transfers", importOptions = DontIncludeTests.class)
 public class LayerArchitectureTest {
+
+    private final static Set<String> ALL_ADAPTER_PACKAGES = getAdapterPackages();
 
     @ArchTest
     public static final ArchRule layersShouldHaveProperDependencies =
@@ -27,5 +37,38 @@ public class LayerArchitectureTest {
                     .whereLayer("Domain").mayOnlyBeAccessedByLayers("Application", "Infrastructure")
 
                     .ignoreDependency(MoneyTransfersApplication.class, MoneyTransfersAPI.class);
+
+    @ArchTest
+    public void adaptersShouldNotDependOnEachOther(JavaClasses classes) {
+        ALL_ADAPTER_PACKAGES.forEach(adapterPackage -> {
+
+            final String[] otherAdapterPackages = ALL_ADAPTER_PACKAGES.stream()
+                    .filter(aPackage -> !aPackage.equals(adapterPackage))
+                    .toArray(String[]::new);
+
+            noClasses()
+                    .that()
+                    .resideInAPackage(adapterPackage)
+                    .should()
+                    .accessClassesThat()
+                    .resideInAnyPackage(otherAdapterPackages)
+                    .check(classes);
+
+        });
+    }
+
+    private static Set<String> getAdapterPackages() {
+        Set<ClassPath.ClassInfo> classInfos = null;
+        try {
+            classInfos = ClassPath.from(MoneyTransfersApplication.class.getClassLoader())
+                    .getTopLevelClassesRecursive("com.krzykrucz.transfers.infrastructure");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return classInfos.stream()
+                .map(ClassPath.ClassInfo::getPackageName)
+                .filter(aPackage -> !aPackage.equals("com.krzykrucz.transfers.infrastructure"))
+                .collect(Collectors.toSet());
+    }
 
 }
